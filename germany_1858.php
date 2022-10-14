49e9f47eb53d2f6d3486ec54a8d33dca9dfaa526
Timothy Jesionowski <timothy.n.jesionowski@vertaeon.com>
2022-10-14 17:52:08 -0500

    Made the journal title a php variable set at the top of the file.

--- germany_1858.php
+++ germany_1858.php
@@ -1,7 +1,14 @@
 <!DOCTYPE html>
+<?php
+ $journal = "Germany 1858";
+?>
 <html>
   <head>
-    <title>Germany 1858</title>
+    <title>
+      <?php
+       echo $journal;
+       ?>
+    </title>
     <link href="https://georgeeliotarchive.org/themes/gearchive-theme/css/style.css?v=3.0.2" media="all" rel="stylesheet" type="text/css">
     <link href="https://netdna.bootstrapcdn.com/font-awesome/3.2.1/css/font-awesome.css" media="all" rel="stylesheet" type="text/css">
     <link href="journal.css" media="all" rel="stylesheet" type="text/css">
@@ -69,7 +76,7 @@ function getJournalEntries($data) {
 	foreach($data as $val){
 		$is_germ = false;
 		foreach($val['element_texts'] as $v){
-			if(strlen($v['text']) < 30 && strtolower($v['text']) == 'germany 1858'){
+			if(strlen($v['text']) < 30 && strtolower($v['text']) == strtolower($journal)){
 				$is_germ = true;
 				break;
 			}
