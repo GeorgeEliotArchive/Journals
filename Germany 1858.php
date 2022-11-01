a37b1b57710383659c0232223d4df553cb2088a3
Timothy Jesionowski <timothy.n.jesionowski@vertaeon.com>
2022-11-01 12:48:35 -0500

    Updated the citation formatting.

--- Germany 1858.php
+++ Germany 1858.php
@@ -183,8 +183,8 @@ $months = $newMonths;
         <details>
           <summary>
             <?php echo getElement($entry, "Date"); ?>
-            -- Journal Entry (Harris &amp; Johnston, 
-            <i><?php echo $journal; ?></i>: <?php echo getElement($entry, "Source"); ?>.)
+            -- Journal Entry (Harris &amp; Johnston, <em>Journals</em>, 
+            <?php echo $journal; ?>: <?php echo getElement($entry, "Source"); ?>.)
           </summary>
           <div><?php echo getElement($entry, "Journal Entry"); ?></div>
         </details>
