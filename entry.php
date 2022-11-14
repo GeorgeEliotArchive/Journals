bb25fa21a0cac27a4dce99bd070cb452d375724d
miura5 <mathewnagle888@gmail.com>
2022-11-13 20:22:36 -0600

    Add button to generate and copy entry links

--- entry.php
+++ entry.php
@@ -1,8 +1,19 @@
-<details id="<?php echo $year.'-'.date('m',strtotime($month)).'-'.getDay($entry) ; ?>">
+<?php $anchor = $year.'-'.date('m',strtotime($month)).'-'.getDay($entry) ; ?>
+<details id="<?php echo $anchor; ?>">
   <summary>
     <?php echo getElement($entry, "Date"); ?>
     -- Journal Entry (Harris &amp; Johnston, <em>Journals</em>, 
     <?php echo getElement($entry, "Source"); ?>: <?php echo getElement($entry, "Page"); ?>.)
+    <button id="<?php echo $anchor; ?>" onClick="replyId(this.id)">&#10697;</button>
   </summary>
   <div><?php echo getElement($entry, "Journal Entry"); ?></div>
 </details>
+
+<script>
+  // https://stackoverflow.com/q/4825295
+  function replyId(clicked_id) {
+    // removes anchor (if any) and adds anchor
+    let url = window.location.href.split('#')[0] + "#" + clicked_id;
+    window.prompt("Copy to clipboard: Ctrl+C, Enter", url);
+  }
+</script>
